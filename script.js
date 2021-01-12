9237b34edc0fff3d8db71765d215da4dbcfc6632
Sasha Avreline <savreline@gmail.com>
2021-01-11 20:41:25 -0800

    Fixed nav highlight error

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
 $(function(){
-    $("#header").load("/header.html"); 
-    $("#nav").load("/nav.html");
-    $("#footer").load("/footer.html"); 
+  $("#header").load("/header.html"); 
+  $("#nav").load("/nav.html", highlightNav);
+  $("#footer").load("/footer.html"); 
+});
+
+// https://www.sitepoint.com/community/t/highlight-the-active-link-in-a-navigation-menu/38169
+function highlightNav() {
+  var elements = document.getElementById("toggleNav").children[0].children
+  for (var i = 0; i < elements.length; i++) {
+      var url = document.URL;
+      var href = elements[i].href;
+      var index = url.indexOf("/", 22);
+      url = url.substring(22, index);
+      if ((index == -1 && elements[i].innerHTML == "Home")
+          || (index != -1 && href.includes(url)))
+          document.links[i].className += " active";
+  }
+}
+
+var interns = "internships from 2006-2009"
+var courses = "Selected Courses"
+function toggle(obj, text) {
+  obj.innerHTML = (obj.innerHTML == "Hide "+text) ? "Show "+text : "Hide "+text ;
+}
+
+var shiftWindow = function() { scrollBy(0, -50) };
+window.addEventListener("hashchange", shiftWindow);
+function load() { if (window.location.hash) shiftWindow(); }
+
+// https://stackoverflow.com/questions/31100690/using-bootstrap-data-toggle-collapse-and-scrollintoview-to-show-and-scroll-t
+window.onload = function () {
+  $('#mathCourses').on('shown.bs.collapse', function () {
+    this.scrollIntoView();
   });
+  $('#bcsCourses').on('shown.bs.collapse', function () {
+    this.scrollIntoView();
+  });
+}
